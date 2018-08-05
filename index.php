f93898b773fe91f5ab75e88f46c92380078f40b4
Brandon Tesovic <brandonwebhost@gmail.com>
2018-08-05 13:57:26 +1000

    commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <!-- Bootstrap -->
+  
+ <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!--SEO MeteData -->
+    <meta name="description" content="New local Comic Traders is a new store featuring comic books, retro video games and arcade machines. Come see for a peak of our latest games.
+    Check out our site for what we have in store!">
+    <meta name="keywords" content="Gaming Robina, arcade machines, video games, Robina, Gold Coast Comics">
+    <meta name="author" content="Arcade Gaming Robina">
+    <!--Title-->
+<title>Brandon Tesovic Resume</title>
+<!--Link-->
+<link href="css/font-awesome.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<script src="main.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> 
+<!--End of Linking-->
+
+</head>
+<body>
+
+    <img class="bt"src="img/bt.png" alt="Brandon Tesovic">
+
+
+
+
+     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    
+</body>
